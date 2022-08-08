c8daaf0a3ff0efd19f9b76a343ea637bcb918e79
John Clarke <john@algofi.org>
2022-08-08 15:42:18 +0000

    update

--- algofi_amm/__init__.py
+++ algofi_amm/__init__.py
@@ -3,5 +3,5 @@ This module contains all the relevant classes and data for interacting with the 
 """
 
 __all__ = ["v0", "contract_strings", "utils"]
-__version__ = "1.0.5"
+__version__ = "1.1.0"
 __author__ = "Algofi"
